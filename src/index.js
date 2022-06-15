460613028ccd887f48980a8717a4fe186ec45242
Mark <mgalex12@icloud.com>
2022-06-15 15:25:32 -0400

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+   document.querySelector('form').addEventListener('submit', (e) => {
+     e.preventDefault()
+     handleTask(e.target.new_task_description.value)
+   })
 });
+
+function handleTask(task){
+  let newLi = document.createElement('li')
+  newLi.textContent = task
+  let taskElement = document.querySelector('#tasks')
+  taskElement.appendChild(newLi)
+}
